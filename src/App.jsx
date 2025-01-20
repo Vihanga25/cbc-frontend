2e027b696f5e688c6bf46c5dda760ed55c1791f2
Vihanga Lahiru <is2019193@mgt.rjt.ac.lk>
2025-01-20 22:58:46 +0530

    Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+import LoginPage from './pages/loginpage'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <div>
+      <LoginPage/>
+
+    </div>
    
   )
 }
---
+++ src/pages/loginpage.jsx
@@ -0,0 +1,13 @@
+export default function LoginPage(){
+
+
+return ( 
+    <div>
+        <h1>Login Page</h1>
+        <input type ="text" placeholder="Enter your username"/>
+        <input type = "Password" placeholder ="Enter your Password"/>
+        <button>Login</button>
+    </div>
+)
+
+}
