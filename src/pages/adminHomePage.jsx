2e84262bbd17e0a783aa22f682ca183124cd2cb9
Vihanga Lahiru <is2019193@mgt.rjt.ac.lk>
2025-01-21 18:35:53 +0530

    admin page changes

--- src/pages/adminHomePage.jsx
+++ src/pages/adminHomePage.jsx
@@ -1,4 +1,8 @@
 import { Link } from "react-router-dom";
+import { BsGraphUp } from "react-icons/bs";
+import { MdProductionQuantityLimits } from "react-icons/md";
+import { AiOutlineProduct } from "react-icons/ai";
+import { IoPeopleSharp } from "react-icons/io5";
 
 export default function AdminHomePage(){
 
@@ -7,10 +11,10 @@ return(
     
     <div className="w-[300px] h-screen bg-blue-600 flex flex-col items-center">
 
-    <Link to ="/admin/dashboard">Dashboard</Link>
-    <Link to ="/admin/products">Products</Link>
-    <Link to ="/admin/orders">Orders</Link>
-    <Link to ="/admin/customers">Customers</Link>
+    <Link className="flex flex-row items-center mb-5 text-slate-200 hover:text-red-400 " to ="/admin/dashboard"><BsGraphUp />Dashboard</Link>
+    <Link className="flex flex-row items-center mb-5 text-slate-200 hover:text-red-400" to ="/admin/products"><AiOutlineProduct />Products</Link>
+    <Link className="flex flex-row items-center mb-5 text-slate-200 hover:text-red-400" to ="/admin/orders"><MdProductionQuantityLimits />Orders</Link>
+    <Link className="flex flex-row items-center mb-5 text-slate-200 hover:text-red-400" to ="/admin/customers"><IoPeopleSharp />Customers</Link>
     </div>
 
     <div className="bg-red-500 w-[80%] h-screen"></div>
