71fef530765b735c99e7ddc55d70cc670011b2e8
Vihanga Lahiru <is2019193@mgt.rjt.ac.lk>
2025-03-07 15:46:51 +0530

    add cart route to HomePage

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -3,6 +3,7 @@ import Header from '../components/header';
 import LoginPage from './loginpage';
 import ProductOverview from './home/productOverview';
 import ProductPage from './home/product';
+import Cart from './home/cart';
 
 
 export default function HomePage() {
@@ -13,7 +14,8 @@ export default function HomePage() {
         <Routes path="/*">
           <Route path="/" element={<h1>Home Page</h1>} />
           <Route path="/products" element={<ProductPage/>} />
-          <Route path="/login" element={<LoginPage/>} />     
+          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/cart" element={<Cart/>} />      
           <Route path="/productInfo/:id" element={<ProductOverview/>} />
         </Routes>  
       </div>
