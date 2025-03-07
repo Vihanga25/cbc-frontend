28b5e0b8c4b12abda3cb61661b0b02beaac18f7d
Vihanga Lahiru <is2019193@mgt.rjt.ac.lk>
2025-03-07 16:32:44 +0530

    refactor media upload utility for improved readability

--- src/utils/mediaUpload.js
+++ src/utils/mediaUpload.js
@@ -8,27 +8,25 @@ const Url = "https://dvqvpetaaijkbamujmkt.supabase.co";
 const supabase = createClient(Url, key);
 
 export default function uploadMediaToSupabase(file) {
-    return new Promise((resolve, reject) => {
-      if (file == null) {
-        reject("File not added");
-      }
-      let fileName = file.name;
-      const extension = fileName.split(".")[fileName.split(".").length - 1];
-  
-      const timestamp = new Date().getTime();
-  
-      fileName = timestamp +file.name+ "." + extension;
-  
-      supabase.storage.from("images").upload(fileName, file, {
-        cacheControl: "3600",
-        upsert: false,
-      })
-      .then(()=>{
-        const publicUrl = supabase.storage.from("images").getPublicUrl(fileName).data.publicUrl;
-        resolve(publicUrl);
-      })
-      .catch((err)=>{
-        reject(err);
-      });
+  return new Promise((resolve, reject) => {
+    if (file == null) {
+      reject("File not added");
+    }
+    let fileName = file.name;
+    const extension = fileName.split(".")[fileName.split(".").length - 1];
+
+    const timestamp = new Date().getTime();
+
+    fileName = timestamp +file.name+ "." + extension;
+
+    supabase.storage.from("images").upload(fileName, file, {
+      cacheControl: "3600",
+      upsert: false,
+    }).then(()=>{
+      const publicUrl = supabase.storage.from("images").getPublicUrl(fileName).data.publicUrl;
+      resolve(publicUrl);
+    }).catch((err)=>{
+      reject(err);
     });
-  }
+  });
+}
